c55f595df6db0e2a94fe41deadfc674ef4129ab3
tiago.bani <tiago.bani@yahoo.com.br>
2021-04-25 05:12:12 -0300

    fix: add image domains

--- next.config.js
+++ next.config.js
@@ -1,5 +1,12 @@
 module.exports = {
 	images: {
-		domains: ['storage.googleapis.com', 'd3t3ozftmdmh3i.cloudfront.net'],
+		domains: [
+			'storage.googleapis.com',
+			'd3t3ozftmdmh3i.cloudfront.net',
+			'www.omnycontent.com',
+			'image.simplecastcdn.com',
+			'jovemnerd.com.br',
+			'ssl-static.libsyn.com',
+		],
 	},
 }
